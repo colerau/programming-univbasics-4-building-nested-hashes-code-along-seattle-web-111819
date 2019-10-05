86a2d56a4326f98f4208c4138330563356f6c3d0
Cole Rau <colevrau@gmail.com>
2019-10-05 23:18:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        
         },
       :matriarch => {
         #your key/value pairs here
