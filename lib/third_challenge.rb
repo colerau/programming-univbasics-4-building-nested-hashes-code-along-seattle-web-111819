c789cf62bc47abff7d0be29d1bc10a3f287c0380
Cole Rau <colevrau@gmail.com>
2019-10-05 23:18:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,18 +4,6 @@ def third_challenge
       :patriarch => {:name => "Lord Montague", :age => "53"},
       :matriarch => {:name => "Lady Montague", :age => "54}"},
       :hero => {:name => "Romeo", :age => "15", :status => "alive"}
-        :name => {
-          "Romeo"
-        }
-        :age => {
-          "15"
-        }
-        :status => {
-          "alive"
-        }
-      }
-        
-      },
       :hero_friends => []
    }, 
    :capulet => {
